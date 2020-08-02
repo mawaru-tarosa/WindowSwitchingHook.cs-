9a651468a9644d07152fc2f59ae150ef114d615f
mawaru-tarosa <69099990+mawaru-tarosa@users.noreply.github.com>
2020-08-02 14:08:43 +0900

    Add files via upload
    WindowSwitchingHook.csの使い方

---
+++ test/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        WindowSwitchingHook wsh = new WindowSwitchingHook();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                wsh.Window += new WindowSwitchingHook.Callback(WindowHook);
+                wsh.Install();
+            }
+            else
+            {
+                wsh.Uninstall();
+            }
+        }
+
+        private void WindowHook(WindowSwitchEventArgs e)
+        {
+            label1.Text = e.GetProcessName();
+            label2.Text = e.GetTitle();
+            label3.Text = e.GetProcessID().ToString();                        
+        }
+    }
+}
